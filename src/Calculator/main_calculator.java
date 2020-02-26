7a1586f48a49057fba0426eb649e0a4518d5ef31
Francis1002 <61449786+Francis1002@users.noreply.github.com>
2020-02-26 14:44:20 +0530

    initial code

---
+++ src/Calculator/main_calculator.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+public class main_calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calculator cals = new calculator();
+
+	}
+
+}
